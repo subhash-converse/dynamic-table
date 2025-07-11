77503c3caaaaa00fcca5ab19fa86347bb2c96235
Subhash <subhash@conversedatasolutions.com>
2025-07-11 15:39:19 +0530

    layout creation

---
+++ src/app/table/page.tsx
@@ -0,0 +1,16 @@
+import Table from '@/components/table'
+import SidebarLayout from '@/layouts/sidebar-layout'
+import React from 'react'
+
+const page = () => {
+    return (
+        <SidebarLayout>
+            <div className='bg-white'>
+                {/* <Table /> */}
+                sdsd
+            </div>
+        </SidebarLayout>
+    )
+}
+
+export default page
---
+++ src/layouts/sidebar-layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useState } from 'react'
+import { BsArrowLeftCircle } from "react-icons/bs";
+import { cn } from '../../lib/utils';
+
+const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
+    const [open, setOpen] = useState(true)
+    return (
+        // <div className='h-screen bg-yellow-200'>{children}</div>
+        <div className={cn('h-screen bg-white text-black grid duration-500',
+            open ? "grid-cols-[260px_1fr]" : "grid-cols-[60px_1fr]"
+        )}>
+            <div className='grid grid-rows-[60px_1fr] border-r  border-gray-400'>
+                <div className={cn('grid border-b w-full border-gray-400 duration-500' , open ? "grid-cols-[1fr_60px]" : "grid-cols-1")}>
+                    <div className={cn('flex justify-center items-center', !open && "hidden" )}>Top-nav Bar</div>
+                    <div className={cn("flex justify-center items-center cursor-pointer")} onClick={() => setOpen((prev) => (!prev))}><BsArrowLeftCircle /></div>
+                </div>
+                <div className=' flex justify-center items-center whitespace-nowrap overflow-hidden'> Side-nav Bar</div>
+            </div>
+
+            <div className='grid grid-rows-[60px_1fr]'>
+                <div className='border-b border-gray-400 flex justify-center items-center'>Top-nav Bar</div>
+                <div className=''>{children}</div>
+            </div>
+        </div>
+    )
+}
+
+export default SidebarLayout
